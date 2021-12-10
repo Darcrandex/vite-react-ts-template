64039c2b5d503c35400ab6fd369108d37d7b3895
darcrandex <darcrandex@gmail.com>
2021-12-10 10:42:39 +0800

    feat: 更新项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,34 @@
 import * as path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, './')
 
   return {
-    plugins: [react()],
+    plugins: [react(), legacy()],
 
     resolve: {
       alias: { '@': path.resolve(__dirname, './src') },
     },
+
     server: { port: parseInt(env.PORT) || 4000, host: true },
+
+    base: './',
+
+    build: {
+      rollupOptions: {
+        output: {
+          // 把不同的依赖分组打包
+          // 这里需要注意顺序,否则会报错
+          manualChunks: {
+            react: ['react', 'react-dom', 'react-router-dom'],
+            mobx: ['mobx', 'mobx-react-lite'],
+          },
+        },
+      },
+    },
   }
 })
