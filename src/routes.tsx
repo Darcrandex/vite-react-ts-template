aec654824f2f4e18ed67f0f9f9e4c2a4f03ba179
darcrand <darcrandex@gmail.com>
2022-06-12 14:28:22 +0000

    🐞 fix: 修复 react18 children 的报错

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { RouteObject } from 'react-router-dom'
 import Home from './pages/Home'
 import AuthWrapper from './common/AuthWrapper'
 import { Authorization } from './enums'
 
 // 使用局部的 loading 是为了让组件不影响其他已经加载的组件
-const LoadingWrapper: React.FC = ({ children }) => <React.Suspense fallback='loading...'>{children}</React.Suspense>
+const LoadingWrapper: React.FC<PropsWithChildren<unknown>> = ({ children }) => (
+  <React.Suspense fallback='loading...'>{children}</React.Suspense>
+)
 
 interface IRoute extends RouteObject {
   component: React.FC | React.LazyExoticComponent<React.FC> // 解决 element 属性无法直接使用 React.LazyExoticComponent 的问题
