babc95f8bdbe1c81e60bf91e5c31811bf52d0f4f
darcrand <darcrandex@gmail.com>
2023-02-02 13:23:17 +0800

    🦄 refactor: lint

--- src/routes/utils.tsx
+++ src/routes/utils.tsx
@@ -4,7 +4,7 @@ import React, { Suspense } from 'react'
  * @description 每个路由组件自己处理懒加载
  * @param Componet 通过 React.lazy 函数获得的懒加载组件
  */
-export function withSuspense(Componet: React.LazyExoticComponent<React.FC<{}>>) {
+export function withSuspense(Componet: React.LazyExoticComponent<React.FC>) {
   const LazyComponent = () => (
     <Suspense fallback='loading...'>
       <Componet />
